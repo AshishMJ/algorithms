ae36556038b59445672f3fef695ecf730659d2b4
Ashish M <ashish.mahendra@walmartlabs.com>
2022-04-03 11:28:04 +0530

    Backtracking done

--- src/main/java/backtracking/RatMaze.java
+++ src/main/java/backtracking/RatMaze.java
@@ -1,15 +1,10 @@
-/*
 package backtracking;
 
-import static backtracking.RatMaze.DIRECTION.DOWN;
 
 public class RatMaze {
     private int[][] solMatrix;
     private int[][] inputMatrix;
     private int N;
-
-    private static final int INIT_VALUE = 0;
-    private static final int FINAL_VALUE = 1;
 
     public RatMaze(int[][] inputMatrix, int N) {
         this.N = N;
@@ -18,37 +13,60 @@ public class RatMaze {
     }
 
     private void initializeSolutionMatrix() {
-        for (int row = 0; row < N; row++) {
-            for (int col = 0; col < N; col++) {
-                solMatrix[row][col] = INIT_VALUE;
+        solMatrix = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                solMatrix[i][j] = 0;
             }
         }
     }
 
-    public void solve() {
-        if (checkIfRatCanMove(0, 0, DOWN)) {
+    private void printSolution() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(solMatrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
 
+    private boolean isPosEligible(int i, int j) {
+        return (i < N && j < N && inputMatrix[i][j] == 1);
+    }
+
+    public boolean solveMaze(int i, int j) {
+        boolean isPossible = false;
+
+        if (isPosEligible(i, j)) {
+            solMatrix[i][j] = 1;
+
+            if (i == N - 1 && j == N - 1 && inputMatrix[i][j] == 1) {
+                isPossible = true;
+            } else if (solveMaze(i, j + 1)) {
+                isPossible = true;
+            } else if (solveMaze(i + 1, j)) {
+                isPossible = true;
+            }
+
+            if (!isPossible) {
+                solMatrix[i][j] = 0;
+            }
+        }
+        return isPossible;
+    }
+
+
+    public static void main(String args[]) {
+        int[][] maze = {{1, 1, 1},
+                {0, 1, 1},
+                {0, 1, 1}
+        };
+        RatMaze rat = new RatMaze(maze, maze.length);
+        boolean isSolved = rat.solveMaze(0, 0);
+        if (isSolved) {
+            rat.printSolution();
         } else {
-
+            System.out.println("No solution exists for the maze.");
         }
-    }
-
-    private boolean checkIfRatCanMove(int row, int col) {
-        if (row == N - 1 && col == N - 1) {
-            solMatrix[row][col] = FINAL_VALUE;
-            return true;
-        }
-
-        if()
-        return false;
-    }
-
-    private boolean isPositionWithinBoundary(int row, int col) {
-        if (row >= 0 && col >= 0 && row < N && col > N && inputMatrix[row][col] != 0) {
-            return true;
-        }
-
-        return false;
     }
 }
-*/
